666943d108b8533e8cbce8c16981cac83cc0f3d6
Jisse Reitsma <info@yireo.com>
2018-07-20 10:27:26 +0200

    Add exit() to list of exit-functions

--- Extdn/Sniffs/Code/NoDebuggingStatementsSniff.php
+++ Extdn/Sniffs/Code/NoDebuggingStatementsSniff.php
@@ -42,15 +42,16 @@ class NoDebuggingStatementsSniff implements Sniff
                 continue;
             }
 
-            if ($token['type'] == 'T_EXIT' && $token['content'] == 'die') {
+            $exitFunctions = ['exit', 'die'];
+            if ($token['type'] == 'T_EXIT' && in_array($token['content'], $exitFunctions)) {
                 $message = $this->message;
                 $message .= ' Function "die()" was found in the code.';
                 $phpcsFile->addWarning($message, $stackPtr, 'die');
                 continue;
             }
 
-            $debuggingStrings = ['print_r', 'var_export', 'var_dump'];
-            if ($token['type'] == 'T_STRING' && in_array($token['content'], $debuggingStrings)) {
+            $debuggingFunctions = ['print_r', 'var_export', 'var_dump'];
+            if ($token['type'] == 'T_STRING' && in_array($token['content'], $debuggingFunctions)) {
                 $message = $this->message;
                 $message .= ' Function "var_export()" was found in the code.';
                 $phpcsFile->addWarning($message, $stackPtr, 'var_export');
