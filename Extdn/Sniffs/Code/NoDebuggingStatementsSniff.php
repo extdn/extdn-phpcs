603072df810ecb1ed78bffd9500d13c86ace590b
Jisse Reitsma <info@yireo.com>
2018-07-20 11:03:41 +0200

    Refactor to use findNext() instead for better performance

--- Extdn/Sniffs/Code/NoDebuggingStatementsSniff.php
+++ Extdn/Sniffs/Code/NoDebuggingStatementsSniff.php
@@ -36,28 +36,48 @@ class NoDebuggingStatementsSniff implements Sniff
     public function process(File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
-
-        foreach ($tokens as $token) {
-            if (!isset($token['type']) || !isset($token['content'])) {
-                continue;
+        $types = [T_EXIT, T_STRING];
+        while ($stackPtr = $phpcsFile->findNext($types, $stackPtr)) {
+            if (!$stackPtr || !isset($tokens[$stackPtr])) {
+                break;
             }
 
-            $exitFunctions = ['exit', 'die'];
-            if ($token['type'] == 'T_EXIT' && in_array($token['content'], $exitFunctions)) {
-                $message = $this->message;
-                $message .= ' Function "die()" was found in the code.';
-                $phpcsFile->addWarning($message, $stackPtr, 'die');
-                continue;
-            }
-
-            $debuggingFunctions = ['print_r', 'var_export', 'var_dump'];
-            if ($token['type'] == 'T_STRING' && in_array($token['content'], $debuggingFunctions)) {
-                $message = $this->message;
-                $message .= ' Function "var_export()" was found in the code.';
-                $phpcsFile->addWarning($message, $stackPtr, 'var_export');
-                continue;
-            }
+            $token = $tokens[$stackPtr];
+            $this->processToken($phpcsFile, $stackPtr, $token);
+            $stackPtr++;
         }
+    }
+
+    /**
+     * @param File $phpcsFile
+     * @param int $stackPtr
+     * @param array $token
+     *
+     * @return bool
+     */
+    private function processToken(File $phpcsFile, int $stackPtr, array $token): bool
+    {
+        if (!isset($token['type']) || !isset($token['content'])) {
+            return false;
+        }
+
+        $exitFunctions = ['exit', 'die'];
+        if ($token['type'] == 'T_EXIT' && in_array($token['content'], $exitFunctions)) {
+            $message = $this->message;
+            $message .= sprintf(' Function "%s()" was found.', $token['content']);
+            $phpcsFile->addWarning($message, $stackPtr, $token['content']);
+            return true;
+        }
+
+        $debuggingFunctions = ['print_r', 'var_export', 'var_dump'];
+        if ($token['type'] == 'T_STRING' && in_array($token['content'], $debuggingFunctions)) {
+            $message = $this->message;
+            $message .= sprintf(' Function "%s()" was found.', $token['content']);
+            $phpcsFile->addWarning($message, $stackPtr, $token['content']);
+            return true;
+        }
+
+        return false;
     }
 }
 
